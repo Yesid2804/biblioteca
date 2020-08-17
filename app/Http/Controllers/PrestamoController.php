4ad515c8482ae14beab0eff6d79b892ce8c5613c
Alcalyx12 <duvantitan@gmail.com>
2020-08-17 15:31:55 -0500

    controllersmodifs

--- app/Http/Controllers/PrestamoController.php
+++ app/Http/Controllers/PrestamoController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpFoundation\Response;
 
 class PrestamoController extends Controller
 {
